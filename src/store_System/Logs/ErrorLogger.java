d00b913fa706b9dd168da71e0d67b0ff74c2a863
shaharewq0 <cohenhas@bgu.post.ac.il>
2020-04-08 13:45:36 +0300

    added ErrorLogger class

---
+++ src/store_System/Logs/ErrorLogger.java
@@ -0,0 +1,45 @@
+package store_System.Logs;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ErrorLogger implements Log {
+    private static EventLogger INSTANCE;
+    private static File log;
+    private static FileWriter fileWriter;
+
+    @Override
+    public void Log(String msg) {
+        try {
+            fileWriter.write(msg);
+        } catch (IOException e) {
+            System.out.println("error in printing to the error-log file");
+        }
+    }
+
+    @Override
+    public void CloseLogger()  {
+        try {
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("error in closing the error-logger");
+        }
+    }
+
+    @Override
+    public Log GetInstance() {
+        if (INSTANCE == null)
+        {
+            log = new File("/LOG/Error_Log.txt");
+            try {
+                log.createNewFile();
+                fileWriter = new FileWriter("/LOG/Error_log.txt");
+            }
+            catch (IOException e){
+                System.out.println("error in creating error-log file");
+            }
+        }
+        return  INSTANCE;
+    }
+}
